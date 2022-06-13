31ce050ec4369d2483060e8175c5a77875cfecfe
Tom Mills <t.mills124@gmail.com>
2022-06-13 18:20:17 +0100

    added testing and additional steps to growUp function to adjust hunger and fitness counters

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -12,19 +12,37 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
+    it('has an initial hunger of zero', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+    it('has an initial fitness of ten', () => {
+        const pet = new Pet('Fido');
+        expect(pet.fitness).toEqual(10);
+    });
 });
 
 describe('growUp', () => {
     it('adds one to age' , () => {
-    const pet = new Pet('Fido');
-    pet.growUp();
-    expect(pet.age).toEqual(1);
-})
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    })
     it('adds two to age' , () => {
-    const pet = new Pet('Fido');
-    pet.growUp();
-    pet.growUp();
-    expect(pet.age).toEqual(2);
-})
+        const pet = new Pet('Fido');
+        pet.growUp();
+        pet.growUp();
+        expect(pet.age).toEqual(2);
+    })
+    it('adds five to hunger', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    })
+    it('decreases fitness by three', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    })
 
 })
