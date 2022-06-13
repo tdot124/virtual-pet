38a397c422865798fec476a439b3290b25ca84d4
Tom Mills <t.mills124@gmail.com>
2022-06-13 17:18:20 +0100

    Added test to check name is assigned and added code to allow for naming of pet

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -4,4 +4,7 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('returns an object with given pet name', () => {
+        expect(new Pet('Fido')).toEqual({name: 'Fido'})
+    })
 });
